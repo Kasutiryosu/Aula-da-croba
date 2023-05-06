18de7c2f74acdd2a07e66ee5388b6655be70d097
Kasutiryosu <130712341+Kasutiryosu@users.noreply.github.com>
2023-05-06 11:57:40 -0300

    Delete exercício 5.py

--- exercício 5.py
+++
@@ -1,4 +0,0 @@
-C= int(input("insira uma quantia de graus celsius"))
-print(C ,"graus Celsius é o equivalente à", C+273.15 ,"Kelvin")
-
-print(C ,"graus Celsius é o equivalente à", (C/5)*9+32 ,"Fahrenheit")
