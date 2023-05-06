e41ca9e6f1fbf74567f888db4571f76f89d83014
Kasutiryosu <130712341+Kasutiryosu@users.noreply.github.com>
2023-05-06 11:54:41 -0300

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,6 @@
+#  == verifica se duas variáveis são iguais
+#  != verifica se dois valores são diferentes
+#  > verifica se um valor maior que outro  
+#  < verifica se um valor é menor que outro
+#  >= verifica se um valor é maior ou igual ao outro
+#  <= verifica se um valor é menor ou igual ao outro
