0a9edf44b43eaff45d498598140385bc9192078c
Kasutiryosu <130712341+Kasutiryosu@users.noreply.github.com>
2023-05-06 11:57:18 -0300

    Delete exercício 2.py

--- exercício 2.py
+++
@@ -1,7 +0,0 @@
-numero1= int(input("Escolha um número"))
-numero2= int( input("Escolha outro número"))
-
-print("Soma", numero1+numero2)
-print("Subtração", numero1-numero2)
-print("Divisão", numero1/numero2)
-print("Multiplicação", numero1*numero2)
